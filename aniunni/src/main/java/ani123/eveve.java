238aff1077e1aab0f3b7238d6f56949b0483eaf2
Frazina <Frazina@LAPTOP-T5HRFLI1>
2024-08-23 06:46:05 +0530

    ufrt

---
+++ aniunni/src/main/java/ani123/eveve.java
@@ -0,0 +1,18 @@
+package ani123;
+
+public class eveve {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int no=24;
+if(no%2==0)
+{
+	System.out.println("no is even" +no);
+}
+else
+{
+	System.out.println("no is odd" +no);
+}
+	}
+
+}
