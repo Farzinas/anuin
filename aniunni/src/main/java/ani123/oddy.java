e3b3f2b040fb87703099a560f1d9283a804c1d3e
Frazina <Frazina@LAPTOP-T5HRFLI1>
2024-08-24 13:07:41 +0530

    ghbsdb

---
+++ aniunni/src/main/java/ani123/oddy.java
@@ -0,0 +1,16 @@
+package ani123;
+
+public class oddy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=20,b=10;
+		int c=a%b;
+		System.out.println(c);
+		
+		
+		
+
+	}
+
+}
